b8aea426235ebe6ed0b62c7a27d59378b542ce7d
Tomas Frydrych <tomas@tomtain.com>
2021-06-24 18:36:13 +0100

    new conan config

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, tools
 from conans.errors import ConanInvalidConfiguration
 
 class VstudmConan(ConanFile):
-    python_requires = "vst-conan-helper/0.8@vernierst+vst-libs/main"
+    python_requires = "vst-conan-helper/0.9@vernierst+vst-libs/main"
     python_requires_extend = "vst-conan-helper.VstConanBase"
 
     name = "openh264"
